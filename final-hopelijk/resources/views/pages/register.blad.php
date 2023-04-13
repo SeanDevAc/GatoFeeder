e31159c0204f74a6cd2d771d0407945210812a95
wimb64 <wimb64@icloud.com>
2023-04-13 19:27:14 +0200

    removed old stuff

--- final-hopelijk/resources/views/pages/register.blad.php
+++
@@ -1,8 +0,0 @@
-<form action="/api/register" method="post">
-    <p>login</p>
-    <label for="email">email:</label>
-    <input type="text" name="email" id="email">
-    <label for="password">password:</label>
-    <input type="password" name="password" id="password">
-    <input type="submit" value="hoi">
-</form>
