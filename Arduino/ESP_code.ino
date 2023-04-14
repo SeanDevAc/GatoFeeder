07ac32deff226fc1b91c4866df2e63f47a76ea66
baskortenoever <106408136+baskortenoever@users.noreply.github.com>
2023-04-14 11:35:06 +0200

    Update ESP_code.ino
    code aangepast voor andere motor(NEMA 17) en voor de load cell

--- Arduino/ESP_code.ino
+++ Arduino/ESP_code.ino
@@ -11,25 +11,28 @@
 //stepper libary
 #include <Stepper.h> 
 
-const int stepsPerRevolution = 1000; //snelheid van stepper
+const int steps_per_rev = 200; //Set to 200 for NIMA 17
+#define IN1 14
+#define IN2 27
+#define IN3 26
+#define IN4 25
 
-// ULN2003 Motor Driver Pins. de aanstuurding tussen ESP en stepper
-#define IN1 19
-#define IN2 18
-#define IN3 5
-#define IN4 17
 
 // initialize the stepper library
-Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
+Stepper motor(steps_per_rev, IN1, IN2, IN3, IN4);
+
 
 //gewichtsensor
 #include "soc/rtc.h"
 #include "HX711.h"
 
 // HX711 circuit wiring.  aanstuurding tussen weegschaal en ESP
- const int LOADCELL_DOUT_PIN = 16;
- const int LOADCELL_SCK_PIN = 4;
- 
+ const int LOADCELL_DOUT_PIN_1KG = 16;
+ const int LOADCELL_SCK_PIN_1KG = 4;
+
+int old_stock_weight = 0;
+int old_tray_weight = 0;
+
  HX711 scale;
 
 //ezButton button(23);
@@ -39,8 +42,7 @@ Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
 WiFiMulti wifiMulti;
 
 
-//const int LED_PIN = 5;
-//int led_state = 0;
+
 
 
 //long start_time=0;
@@ -48,47 +50,47 @@ WiFiMulti wifiMulti;
 
 void read_scale() {//voor de setup van de weegschaal. wordt aangeroepen in setup
                       
-    USE_SERIAL.begin(115200);
-    Serial.begin(115200);
-    Serial.println("HX711 Demo");
-    Serial.println("Initializing the scale");
+  Serial.begin(115200);
+  Serial.println("HX711 Demo");
+  Serial.println("Initializing the scale");
 
-    scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.begin(LOADCELL_DOUT_PIN_1KG, LOADCELL_SCK_PIN_1KG);
+  
 
-    Serial.println("Before setting up the scale:");
-    Serial.print("read: \t\t");
-    Serial.println(scale.read());      // print a raw reading from the ADC
+  Serial.println("Before setting up the scale:");
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());      // print a raw reading from the ADC
 
-    Serial.print("read average: \t\t");
-    Serial.println(scale.read_average(20));   // print the average of 20 readings from the ADC
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));   // print the average of 20 readings from the ADC
 
-    Serial.print("get value: \t\t");
-    Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight (not set yet)
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight (not set yet)
 
-    Serial.print("get units: \t\t");
-    Serial.println(scale.get_units(5), 1);  // print the average of 5 readings from the ADC minus tare weight (not set) divided
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);  // print the average of 5 readings from the ADC minus tare weight (not set) divided
             // by the SCALE parameter (not set yet)
             
-    scale.set_scale(-1965);
-      //scale.set_scale(-471.497);                      // this value is obtained by calibrating the scale with known weights; see the README for details
-    scale.tare();               // reset the scale to 0
+  scale.set_scale(-1965);
+  //scale.set_scale(-471.497);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+  scale.tare();               // reset the scale to 0
 
-    Serial.println("After setting up the scale:");
+  Serial.println("After setting up the scale:");
 
-    Serial.print("read: \t\t");
-    Serial.println(scale.read());                 // print a raw reading from the ADC
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());                 // print a raw reading from the ADC
 
-    Serial.print("read average: \t\t");
-    Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
 
-    Serial.print("get value: \t\t");
-    Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight, set with tare()
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));   // print the average of 5 readings from the ADC minus the tare weight, set with tare()
 
-    Serial.print("get units: \t\t");
-    Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
             // by the SCALE parameter set with set_scale
 
-    Serial.println("Readings:");
+  Serial.println("Readings:");
 }
 
 int print_scale_readings() {//geeft de value terug van de weegschaal als int.
@@ -97,126 +99,39 @@ int print_scale_readings() {//geeft de value terug van de weegschaal als int.
    Serial.print(value);
    Serial.print("\t| average:\t");
    Serial.println(int(scale.get_units(10)), 5);
-   delay(5000);
+   delay(2000);
    return value * 10;
-}
+  }
+
+
 
 void rotate_stepper_motor() {//stepper motor aansturing
-  // step one revolution in one direction:
-  
-  Serial.println("met de klok mee");
-  myStepper.step(stepsPerRevolution);
 
-  // step one revolution in the other direction:
-  Serial.println("tegen de klok in");
-  myStepper.step(-stepsPerRevolution);
-
-  
-  
+  Serial.println("Rotating Clockwise...");
+  motor.step(steps_per_rev);
+    
 }
 
 
-
-void get_feed_now_state(){//feed state krijgen denk ik
-  
-}
 
 void setup() {
 
     read_scale();//roep scale aan
+    
+    motor.setSpeed(30);//aantal stappen
+
+    Serial.begin(115200);
+
  
     
-    for(uint8_t t = 4; t > 0; t--) {
-        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
-        USE_SERIAL.flush();
-        delay(1000);
-
-        // set the speed in rpm. //stepper. 
-        myStepper.setSpeed(5);
-
-    }
 }
-
-// dit stuk is voor draadloze verbinding
-void initWifi() {//connect met wifi
-  if (WiFi.status() != WL_CONNECTED) {
-    WiFi.mode(WIFI_STA);
-    WiFi.begin("baspi", "banaan");//hier kan je de raspberry pi dingen wijzigen.
-    USE_SERIAL.println("trying to connect");
-  }
-  sleep(100);
-  if (WiFi.status() == WL_CONNECTED) {
-    USE_SERIAL.print("connected");
-    sleep(200);
-  }
-  USE_SERIAL.print("hoi2");
-  return;
-}
-
-
-
 
 void loop() {
 
   print_scale_readings();
-
-  //rotate_stepper_motor();
+ 
+  rotate_stepper_motor();
 
 }
-
-// //vanaf line 177 code om later te toevoegen
-
-//   //wifiMulti.addAP("meme_device", "driekeerhoi");
-    
-//     //start_time = millis();
-// //void set_remote_button(){}
-
-
-// // komt later in gebruik
-// // void set_stock_weight_test(int weight) {
-// //   USE_SERIAL.println("stock weight functie gestart");
-// //   if (WiFi.status() != WL_CONNECTED) {
-// //     USE_SERIAL.println("not connected, returning");
-// //     return;
-// //   }
-
-// //   USE_SERIAL.println("connected, continuing");
-// //   HTTPClient http;
-// //   http.begin("http://baspi.local/set_stock_weight/" + weight);
-// //   int httpCode = http.GET();
-// //   if (httpCode > 0) {
-// //     USE_SERIAL.println("iets mis");
-// //     if (httpCode == HTTP_CODE_OK) {
-// //       String payload = http.getString();
-// //       USE_SERIAL.println(payload);
-// //     }
-    
-// //   }
-// //   http.end();
-// // }
-
-// // komt later in gebruik
-// // void set_stock_weight(int weight) {
-// //   USE_SERIAL.println("functie gestart");
-// //   if(wifiMulti.run() == WL_CONNECTED) {
-// //     USE_SERIAL.println("wifi is connected") ;
-// //     HTTPClient http;
-// //     http.begin("http://baspi.local/set_stock_weight/" + weight);//link gaat hier anders zijn
-// //     int httpCode = http.GET();
-// //       if(httpCode > 0) {
-// //         USE_SERIAL.println("uhh idk iets mis");  
-// //         if(httpCode == HTTP_CODE_OK) { String payload = http.getString(); }
-// //       }
-// //     http.end();
-// //   }
-// //   USE_SERIAL.println("wifi is denk ik niet connected");
-// // }
-
-//   //dit stuk hoort bij loop maar was voor nu niet nodig.
-//     // USE_SERIAL.println("");
-//     // sleep(1000);
-//     // set_stock_weight(100);
-//     // initWifi();
-//     //set_stock_weight_test(100);
 
 
